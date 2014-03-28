dc0677b61c8638c09d89a48aceabccb35fd49546
MGalindo <mgalindob@gmail.com>
2014-03-28 15:33:55 -0500

    Added car Entry

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,14 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope', function($scope) {
+
+    $scope.car = {make: 'Honda',
+                  model: 'Civic',
+                  year: 2014,
+                  price: 24000
+                 }
+
 
   }])
   .controller('MyCtrl2', [function() {
